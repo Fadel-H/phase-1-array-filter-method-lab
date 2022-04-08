869220b56c1c4aac2d9bb910789635f9d2b963c6
Fadel <fadel.g.heji@gmail.com>
2022-04-07 18:11:41 -0700

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+
+function findMatching(drivers, stringArgument){
+    return drivers.filter(drivers => drivers.toLowerCase() === stringArgument.toLowerCase() )
+}
+
+function fuzzyMatch(drivers, stringArgument){ 
+    return drivers.filter(drivers => drivers.substring(0,stringArgument.length) === stringArgument)
+}
+
+function matchName(drivers, stringArgument){
+    return drivers.filter(drivers => drivers.name === stringArgument)
+}
